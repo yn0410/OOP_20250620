83e6e281f4bc515f91624be550fedfbcea3f4c60
yn <jenny783113@gmail.com>
2025-06-20 10:58:46 +0800

    物件導向 實作(start)

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>物件導向(OOP)</title>
+</head>
+
+<body>
+    <?php
+    class Person {
+        // 成員，屬性
+        public $name;
+        public $age;
+
+        // 方法，行為，建構函式 &__construct=建構子
+        public function __construct($name, $age){
+            $this->name = $name;
+            $this->age = $age;
+        }
+
+        // 方法
+        public function greet(){
+            echo "Hello, my name is {$this->name} and I am {$this->age} years old.<br>";
+        }
+        
+    }
+
+    $jason = new Person('jason', 18);
+    echo $jason->name;
+    echo "<br>";
+    echo $jason->age;
+    echo "<br>";
+    $jason->greet();
+
+    ?>
+</body>
+
+</html>
