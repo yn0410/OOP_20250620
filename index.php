14ef295bb2af837a2916154a7a4c9af9027cc063
yn <jenny783113@gmail.com>
2025-06-20 11:25:50 +0800

    OOP 封裝

--- index.php
+++ index.php
@@ -12,8 +12,12 @@
     // class=類別
     class Person {
         // 成員，屬性
-        public $name;
-        public $age;
+        /* public 
+         * protected
+         * private
+         */
+        protected $name;
+        protected $age;
 
         // 方法，行為，建構函式 &__construct=建構子
         public function __construct($name, $age){
@@ -25,13 +29,36 @@
         public function greet(){
             echo "Hello, my name is {$this->name} and I am {$this->age} years old.<br>";
         }
+        public function getName(){
+            return $this->name;
+        }
+        public function getAge(){
+            return $this->age;
+        }
+        public function setName($name){
+            $this->name = $name;
+        }
+        public function setAge($age){
+            $this->age = $age;
+        }
         
     }
 
     $jason = new Person('jason', 18); //$jason是個物件(?)
-    echo $jason->name;
+    // echo $jason->name;
+    echo $jason->getName();
     echo "<br>";
-    echo $jason->age;
+    // echo $jason->age;
+    echo $jason->getAge();
+    echo "<br>";
+    $jason->greet();
+
+    echo "<hr>";
+    // $jason->name = "Mary";
+    $jason->setName("Mary");
+    echo $jason->getName();
+    echo "<br>";
+    echo $jason->getAge();
     echo "<br>";
     $jason->greet();
 
